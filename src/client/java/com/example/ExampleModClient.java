88cf47a82105c52de304ff086da4e337911130e8
TinyPandas <chris.barwick16@gmail.com>
2023-06-21 10:32:29 -0700

    Merge bucket ColorProviders into one

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -6,8 +6,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> ((CustomBucket) ExampleMod.RED_WATER_BUCKET).getColor(tintIndex), ExampleMod.RED_WATER_BUCKET);
-		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> ((CustomBucket) ExampleMod.GREEN_WATER_BUCKET).getColor(tintIndex), ExampleMod.GREEN_WATER_BUCKET);
-		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> ((CustomBucket) ExampleMod.BLUE_WATER_BUCKET).getColor(tintIndex), ExampleMod.BLUE_WATER_BUCKET);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> ((CustomBucket) stack.getItem()).getColor(tintIndex),
+				ExampleMod.RED_WATER_BUCKET, ExampleMod.GREEN_WATER_BUCKET, ExampleMod.BLUE_WATER_BUCKET);
 	}
 }
